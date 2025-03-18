e75e8c188aea6f19b184010921cb35672679f3eb
Daniel Minichetti <daniel.minichetti@gmail.com>
2025-03-17 21:37:23 -0700

    Create merge_strings_alternately.py

---
+++ LeetCode 75/Array & Strings/merge_strings_alternately.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        # developed a empty list called merged to store the merged characters from word 1 and 2
+        merged = []
+
+        # run a foor loop to iterate over corresponding characters of word1 and word 2
+        for a, b in zip(word1, word2):
+            # append concatenation to the merged list
+            merged.append(a + b)
+
+        # after loop, append remaining characters of word1 and word 2 that are not covered by the 
+        # the loop. 
+        merged.append(word1[len(word2):])
+        merged.append(word2[len(word1):])
